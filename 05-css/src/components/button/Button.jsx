f2c8a52dedb0fad953f5db5e2825852cbc016272
Faiz Khan <pfaizkhan95@gmail.com>
2026-01-08 19:06:56 +0530

    new file is here 05-css

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from'../button/button.module.css'
+const Button = () => {
+  return (
+      <div className={styles.btn}>
+      <button >This is button</button>
+      <h1 className={styles.logo}>welcome here Faiz</h1>
+    </div>
+  )
+}
+
+export default Button
+ 
